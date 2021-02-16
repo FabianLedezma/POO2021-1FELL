6b6f1e3d450ed001e25e7bb07e3c9175259ec27a
FabianLedezma <fabianledezma1926@gmail.com>
2021-02-16 11:14:19 -0500

    a

--- Calentamiento C++/Punto1IVA.cpp
+++ Calentamiento C++/Punto1IVA.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main(){
+    float precioNormal;
+    cout<<"Ingrese el precio de tu producto: ";
+    cin>> precioNormal;
     cout<<"Hola mundo";
+    precioNormal = precioNormal + (precioNormal*19/100);
+    cout<<"El precio de su producto mas el IVA es: "<< precioNormal;
     return 0;
 }
